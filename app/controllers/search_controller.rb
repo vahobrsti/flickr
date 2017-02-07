b123be4aa95c0e14f2d0e2bf223659684b4d5ea2
vahobrsti <vahab.rasti@gmail.com>
2017-02-07 22:20:54 +1100

    error handling is fixed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,17 +7,22 @@ class SearchController < ApplicationController
 
     @query = params[:q]
     @page = params[:page]
-    unless @query.nil?
+    unless (@query.nil? || @query.empty?)
       # connect to api and conduct search
       flickr_model = API.new 'c8fa5aa1fe4e498417bedeae59c47636' , '2db20ef953404fdb'
       per_page = 5
       extras='url_t, url_z , url_l'
       page = @page || 1
+
       @results = flickr_model.search_images @query, per_page,extras, page
-      # abort @results.inspect
-      @pages = @results.pages.to_i
-      @total = @results.total.to_i
-      current_user.searches.create(phrase: params[:q])
+      unless @results
+        #got error
+        flash[:message]=flickr_model.error.message
+      else
+        @pages = @results.pages.to_i
+        @total = @results.total.to_i
+        current_user.searches.create(phrase: params[:q])
+      end
     end
   end
 end
--- lib/flickr_gateway.rb
+++ lib/flickr_gateway.rb
@@ -3,17 +3,22 @@ module FlickrGateway
   class API
     require 'flickraw-cached'
 
-    attr_reader :api_key, :secret
+    attr_reader :api_key, :secret, :error
 
     def initialize(api_key, secret)
       @api_key = api_key
       @secret = secret
     end
 
-    def search_images(text, per_page, extras,page)
+    def search_images(text, per_page, extras, page)
       FlickRaw.api_key = @api_key
       FlickRaw.shared_secret = @secret
-      flickr.photos.search text: text, per_page: per_page, extras: extras, page: page
+      begin
+        flickr.photos.search text: text, per_page: per_page, extras: extras, page: page
+      rescue => error
+        @error=error
+        false
+      end
     end
   end
 end
