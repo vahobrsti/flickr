b30a0d6dbfa4bc7fefdbd7e76b729d5f479c1e02
vahobrsti <vahab.rasti@gmail.com>
2017-02-07 23:24:23 +1100

    Recent searches fixed in save time

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,21 +7,24 @@ class SearchController < ApplicationController
 
     @query = params[:q]
     @page = params[:page]
-    unless (@query.nil? || @query.empty?)
+    @recent_searches = current_user.searches.last(10).reverse
+    unless @query.nil? || @query.empty?
       # connect to api and conduct search
-      flickr_model = API.new 'c8fa5aa1fe4e498417bedeae59c47636' , '2db20ef953404fdb'
+      flickr_model = API.new '' , ''
       per_page = 5
-      extras='url_t, url_z , url_l'
+      extras='url_t, url_l'
       page = @page || 1
 
       @results = flickr_model.search_images @query, per_page,extras, page
       unless @results
-        #got error
+        # got error
         flash[:message]=flickr_model.error.message
       else
         @pages = @results.pages.to_i
         @total = @results.total.to_i
+        if params[:page].nil?
         current_user.searches.create(phrase: params[:q])
+        end
       end
     end
   end
